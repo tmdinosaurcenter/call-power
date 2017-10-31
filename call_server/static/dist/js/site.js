174ef1e97d29080bee89bf63041156237eb59d62
Josh Levinger <josh@levinger.net>
2017-10-31 15:14:24 -0700

    Manual sync job schedule

--- call_server/static/dist/js/site.js
+++ call_server/static/dist/js/site.js
@@ -2029,6 +2029,7 @@ $(document).ready(function () {
 
     events: {
       'click .reveal': 'toggleSecret',
+      'submit form.crm-sync': 'syncSubmit',
     },
 
     toggleSecret: function(event) {
@@ -2038,8 +2039,29 @@ $(document).ready(function () {
         } else {
             input.prop('type','password');
         }
-    }
+    },
 
+    syncSubmit: function(event) {
+      event.preventDefault();
+      var form = $(event.target);
+      if (form.hasClass('disabled')) {
+        return false;
+      }
+
+      $.ajax({
+        url: form.attr('action'),
+        method: 'POST',
+        success: function(response) {
+          if (response.scheduled_start_time) {
+            console.log('starting at', response.scheduled_start_time);
+            // put it in the right table cell, disable the button
+            $(form).siblings('.last_sync_time').text(response.scheduled_start_time);
+            $(form).addClass('disabled');
+            $(form).find('button[type="submit"]').addClass('disabled');
+          };
+        }
+      });
+    }
   });
 
 })();

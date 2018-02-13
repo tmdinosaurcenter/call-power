e908b95591d4bb57ca1ded3e78930f8c695a354e
Josh Levinger <josh@levinger.net>
2018-02-12 16:41:08 -0800

    remove missing zipcode tests, now that geocoder is hooked up it may provide a response

--- tests/test_geocoders.py
+++ tests/test_geocoders.py
@@ -27,12 +27,6 @@ class TestGeocoders(BaseTestCase):
         self.assertEqual(result.postal, '94612')
         self.assertEqual(result.state, 'CA')
 
-    def test_geocoder_us_zipcode_does_not_exist_in_local_cache(self):
-        not_a_zipcode = '00000' # non-existant zipcode
-        result = self.us_data._geocoder.postal(not_a_zipcode, provider=self.us_data)
-
-        self.assertEqual(result.postal, None)
-
     def test_geocoder_us_zipcode_exists_live_api(self):
         real_zipcode = '94612'
         result = self.us_data._geocoder.postal(real_zipcode)
@@ -43,16 +37,6 @@ class TestGeocoders(BaseTestCase):
         else:
             self.assertEqual(result.postal, '94612')
             self.assertEqual(result.state, 'CA')
-
-    def test_geocoder_us_zipcode_does_not_exist_live_api(self):
-        not_a_zipcode = '00000' # non-existant zipcode
-        result = self.us_data._geocoder.postal(not_a_zipcode)
-
-        self.assertIsNot(result.service, LOCAL_USDATA_SERVICE)
-        if result.service == 'Timeout':
-            print "geocoder timeout, skipping"
-        else:
-            self.assertEqual(result.postal, None)
 
     def test_geocoder_us_address_exists_live_api(self):
         real_address = '1600 Pennsylvania Ave NW, Washington DC'

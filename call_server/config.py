1c3641fef1d82bf762257c2579e8c2a5824dd25b
Josh Levinger <josh@levinger.net>
2019-03-27 14:46:57 -0700

    bump minor version, the script names are the same but must now be called with `flask` not `python manager.py`

--- call_server/config.py
+++ call_server/config.py
@@ -5,7 +5,7 @@ class DefaultConfig(object):
     PROJECT = 'CallPower'
     DEBUG = False
     TESTING = False
-    VERSION = "1.4.9"
+    VERSION = "1.5.0"
     ENVIRONMENT = os.environ.get('APP_ENVIRONMENT', "Default")
 
     APP_NAME = "call_server"

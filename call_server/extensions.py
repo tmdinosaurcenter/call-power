bc6c900059b3a28f01c9b1ac5c26c637e493d82b
Josh Levinger <josh@levinger.net>
2019-04-10 13:25:34 -0700

    Fix apply_pool_defaults

--- call_server/extensions.py
+++ call_server/extensions.py
@@ -5,8 +5,8 @@ from flask_sqlalchemy import SQLAlchemy as _BaseSQLAlchemy
 # per https://github.com/pallets/flask-sqlalchemy/issues/589#issuecomment-361075700
 class SQLAlchemy(_BaseSQLAlchemy):
     def apply_pool_defaults(self, app, options):
-        super(SQLAlchemy, self).apply_pool_defaults(self, app, options)
         options["pool_pre_ping"] = True
+        super(SQLAlchemy, self).apply_pool_defaults(app, options)
 db = SQLAlchemy()
 
 from flask_caching import Cache

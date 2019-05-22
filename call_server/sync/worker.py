3f0f594c913b66ff3d15e8d3b1ffb59e6e037164
Josh Levinger <josh@levinger.net>
2019-05-22 13:12:49 -0700

    More Heroku logging

--- call_server/sync/worker.py
+++ call_server/sync/worker.py
@@ -1,7 +1,16 @@
 from rq.worker import HerokuWorker
 from flask import current_app
+import logging
+
+logger = logging.getLogger("rq.worker")
+logHandler = logging.StreamHandler()
+logger.addHandler(logHandler)
 
 class CleanSlateHerokuWorker(HerokuWorker):
+    def register_birth(self):
+        logger.info('Registering birth of CleanSlateHerokuWorker %s' % self.name)
+        super(CleanSlateHerokuWorker, self).register_birth()
+
     def execute_job(self, *args, **kwargs):
         # close all the sessions before starting to work
         # to avoid SSL issues with Postgres on second work attempt

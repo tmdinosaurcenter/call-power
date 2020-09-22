ca06b0aad050cf5b5f7827bf0132e7470e14ec2a
Josh Levinger <josh@levinger.net>
2020-09-22 10:14:43 -0700

    release 1.5.4

--- call_server/config.py
+++ call_server/config.py
@@ -5,7 +5,7 @@ class DefaultConfig(object):
     PROJECT = 'CallPower'
     DEBUG = False
     TESTING = False
-    VERSION = "1.5.3"
+    VERSION = "1.5.4"
     ENVIRONMENT = os.environ.get('APP_ENVIRONMENT', "Default")
 
     APP_NAME = "call_server"
